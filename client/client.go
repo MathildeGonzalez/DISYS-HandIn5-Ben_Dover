9070bf77de14940016e1ecd41ebb1b7887002433
Casper2411 <ccaassppeerr2411@gmail.com>
2023-11-22 09:58:18 +0100

    made client work
    coatuhor og det lort

--- client/client.go
+++ client/client.go
@@ -18,22 +18,24 @@ import (
 //A Client has a slice of all replication managers that it connects to and sends requests to
 //When the frontend registers that a replication manager has failed, it removes it from the slice
 
-type Client struct{
-	id 					    int32
+type Client struct {
+	id                  int32
 	replicationManagers []int32
-	auctionClients []proto.AuctionClient
+	auctionClients      []proto.AuctionClient
 }
 
-func main(){
+//make frontent struct
+
+func main() {
 	clientId, _ := strconv.ParseInt(os.Args[1], 10, 32)
 
 	//Create a client struct
 	//add all replication managers to the slice
-	
+
 	client := &Client{
-		id: int32(clientId),
+		id:                  int32(clientId),
 		replicationManagers: []int32{5000, 5001, 5002},
-		auctionClients: []proto.AuctionClient{},
+		auctionClients:      []proto.AuctionClient{},
 	}
 
 	//Connect to all replication managers
@@ -52,29 +54,38 @@ func main(){
 			client.sendBid(int32(bidAmount))
 
 		} else if scan == "result" {
-			//
+			client.getResult()
 		}
 	}
 }
 
-//Function to send bid to all replication managers by looping through auctionClients
-func (client *Client)sendBid(bidAmount int32){
-	for _, auctionClient := range client.auctionClients{
+// Function to send bid to all replication managers by looping through auctionClients
+func (client *Client) sendBid(bidAmount int32) {
+	for _, auctionClient := range client.auctionClients {
 		ack, err := auctionClient.Bid(context.Background(), &proto.BidMessage{Id: client.id, Amount: bidAmount})
 		if err != nil {
-			log.Fatalf("Could not send bid to server: %v", err)
+			log.Fatalf("Could not send bid to server: %v", err) //remove the Server from slice.
 		}
 		log.Printf("Received acknowledgement from server: %v", ack)
 	}
 }
 
-//Function to request result of auction
-func (client *Client)getResult(){
+// Function to request result of auction
+func (client *Client) getResult() {
 	//ask the first replication manager for the result, since it is the first to be updated
+
+	outcome, err := client.auctionClients[0].GetResult(context.Background(), &proto.Empty{})
+	if err != nil {
+		log.Fatalf("Could not receive result from server: %v", err)
+	}
+	if len(outcome.Winner) == 0 {
+		log.Printf("The current highest bid is %d", outcome.HighestBid)
+	} else {
+		log.Printf("The auction is over! The winner is %s, with the bid of: %d", outcome.Winner, outcome.HighestBid)
+	}
 }
 
-
-func (client *Client)connectToServers(){
+func (client *Client) connectToServers() {
 	// Dial the server at the specified port.
 	for _, port := range client.replicationManagers {
 		conn, err := grpc.Dial("localhost:"+strconv.Itoa(int(port)), grpc.WithTransportCredentials(insecure.NewCredentials()))
@@ -87,4 +98,12 @@ func (client *Client)connectToServers(){
 	}
 }
 
-
+// Helper method used to remove a specific element from a Slice
+func removeElement(slice []*proto.AuctionClient, element *proto.AuctionClient) []*proto.AuctionClient {
+	for i, v := range slice {
+		if v == element {
+			return append(slice[:i], slice[i+1:]...)
+		}
+	}
+	return slice // Element not found
+}
